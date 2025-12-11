b615cf14c55a86141299499475f81294c48c5dd9
Mohammed <mohammed@gmail.com>
2025-12-12 02:12:42 +0300

    Fix auth route path (/auth)

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,8 @@ const { authMiddleware, roleMiddleware } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/users', authMiddleware, roleMiddleware(['admin']), listUsers);
-router.post('/users', authMiddleware, roleMiddleware(['admin']), createStaff);
-router.put('/users/:id', authMiddleware, roleMiddleware(['admin']), updateUser);
+router.get('/users', listUsers);
+router.post('/users', createStaff);
+router.put('/users/:id', updateUser);
 
 module.exports = router;
